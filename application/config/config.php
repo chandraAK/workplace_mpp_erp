1212c84f40e4199173c1e21e47a2693dd35464aa
chandraAK <cnsharma@applykart.co>
2026-03-26 16:42:46 +0530

    Changes Config File

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'https://hrms.mahaveergroup.net/';
-$config['base_url'] = 'http://localhost/mpp_erp/';
+$config['base_url'] = 'http://localhost/workplace_mpp_erp/';
 
 /*
 |--------------------------------------------------------------------------
